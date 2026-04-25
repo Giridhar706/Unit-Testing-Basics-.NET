fdbe71c675f22c18326ab0cb4e6c7c44ab050f69
Giridhar Gopal <157975292+Giridhar706@users.noreply.github.com>
2026-04-25 17:27:09 +0530

    Initial Commit
    Created solution and project structure
    Added Calculator class with arithmetic operations
    Implemented division with exception handling
    Added MSTest project and test cases
    Validated all methods using assertions
    Final commit - All tests passing successfully

---
+++ Test1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLibrary;
+using System;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        Calculator calc = new Calculator();
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            Assert.AreEqual(10, calc.Add(5, 5));
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            Assert.AreEqual(2, calc.Subtract(5, 3));
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Assert.AreEqual(15, calc.Multiply(5, 3));
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            Assert.AreEqual(2, calc.Divide(10, 5));
+        }
+
+        [TestMethod]
+        public void TestDivideByZero()
+        {
+            try
+            {
+                calc.Divide(5, 0);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (DivideByZeroException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void TestWithZero()
+        {
+            Assert.AreEqual(5, calc.Add(5, 0));
+        }
+    }
+}
